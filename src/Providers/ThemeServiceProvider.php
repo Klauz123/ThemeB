fd41ed08f9d49843b1a01c5dd3d1f5af1d04e165
klaus.bruns <klaus.bruns@uni-oldenburg.de>
2018-11-15 19:10:36 +0100

    Set Up

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -2,16 +2,40 @@
 
 namespace Theme\Providers;
 
+use IO\Helper\TemplateContainer;
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+
+	}
+
     /**
-     * Register the service provider.
-     */
-    public function register()
+	 * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+           $partial->set('footer', 'Theme::content.ThemeFooter');
+					 $partial->set('navigation', 'Theme::content.ThemeHeaderNavigation');
+
+					 $partial->set('header', 'Theme::content.ThemeHeader');
+
+
+
+
+        }, 0);
+        return false;
 
     }
 }
