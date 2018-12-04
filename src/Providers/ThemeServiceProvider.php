a4ab04cdf76a6945d29dc533d006412c3adc0471
klaus.bruns <klaus.bruns@uni-oldenburg.de>
2018-12-04 22:40:20 +0100

    fixed stuff

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -26,10 +26,9 @@ class ThemeServiceProvider extends ServiceProvider
     {
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
-           $partial->set('footer', 'Theme::content.ThemeFooter');
+
 					 $partial->set('navigation', 'Theme::content.ThemeHeaderNavigation');
 
-					 $partial->set('header', 'Theme::content.ThemeHeader');
 
 
 
